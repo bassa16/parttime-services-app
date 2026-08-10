4a897d75432dd32b36fc100e2e6d5d9a0895ce28
Tharindu Basnayaka <tharindu.basnayaka16@gmail.com>
2026-08-10 19:09:55 +0530

    Update dependencies for core-ktx and lifecycle-runtime-ktx

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.activity:activity-compose:1.13.0")
-    implementation("androidx.core:core-ktx:1.19.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.11.0")
+    implementation("androidx.core:core-ktx:1.18.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
